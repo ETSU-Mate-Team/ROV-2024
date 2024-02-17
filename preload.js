a14588963345d1c0c522fcac0bb0364e3d580386
Nicholas <adverseengineer@gmail.com>
2024-02-17 14:28:28 -0500

    initial commit
    for riley to read

---
+++ preload.js
@@ -0,0 +1,15 @@
+//preload.js
+//Nick Sells
+
+//all the Node.js APIs are available in the preload process.
+//it has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+})
